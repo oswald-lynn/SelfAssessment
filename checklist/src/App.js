d452c31fca10af86071eaee574f0fd174095dea8
Dev Su Su <suphonemyint.la@gmail.com>
2023-01-16 17:44:19 +0630

    test

---
+++ checklist/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CheckList from "./components/CheckList";
+function App() {
+  return (
+    <div className="App">
+      <CheckList />
+    </div>
+  );
+}
+
+export default App;
