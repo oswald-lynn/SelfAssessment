f4c4571b68c8811cfbc4a14f8529677e2fb57804
Dev Su Su <suphonemyint.la@gmail.com>
2023-01-16 18:16:12 +0630

    show courseList data

--- checklist/src/components/CheckList.js
+++ checklist/src/components/CheckList.js
@@ -5,30 +5,42 @@ import { fetchcourseList } from "../actions";
 const CheckList = () => {
   const checkLists = useSelector((state) => state.courseList);
   const dispatch = useDispatch();
-  console.log(checkLists);
+  //console.log(checkLists);
 
   useEffect(() => {
     dispatch(fetchcourseList(checkLists));
-  }, []);
+  }, [dispatch]);
 
   if (!checkLists) {
     return <h2> Loading ...</h2>;
-  } else {
-    console.log(checkLists);
   }
 
   return (
     <div>
       <h2>Check List</h2>
-      {checkLists.id}
-      {checkLists.length > 0
-        ? checkLists.map((clist) => (
-            <div key={clist.id}>
-              {clist.year}
-              {clist.name}
+      <ul>
+        {Object.values(checkLists).map((chlist) => {
+          return (
+            <div key={chlist.id}>
+              <li>
+                <strong>Year</strong> {chlist.year}
+              </li>
+              <li>
+                <strong>Value</strong>
+                {chlist.value}
+              </li>
+              <li>
+                <strong>Name</strong>
+                {chlist.name}
+              </li>
+              <li>
+                <strong>Funding Cap1</strong>
+                {chlist.fundingCap1}
+              </li>
             </div>
-          ))
-        : null}
+          );
+        })}
+      </ul>
     </div>
   );
 };
